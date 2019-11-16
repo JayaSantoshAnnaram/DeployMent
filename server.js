aae4fdaf7944eab5cb1d26c7bcd29b0f454e60ce
JayaSantoshAnnaram <37931170+JayaSantoshAnnaram@users.noreply.github.com>
2019-11-16 13:43:31 +0530

    Build 2

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http=require('http');
 const app=require('./back-end/app');
+const dotenv=require('dotenv');
+
+dotenv.config({path:'./config.env'});
+
 
 
 const server= http.createServer(app);
 
-
-// const port=PORT;
-server.listen(4200,()=>console.log('listen output at localhost:4200'));
+const port=process.env.PORT*1 ||4200;
+server.listen(port,()=>console.log('listen output at localhost:4200'));
 
 
